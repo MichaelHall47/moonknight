958ca800f3ec61dfd4080ae525b5ae4762a20f38
MichaelHall47 <115743182+MichaelHall47@users.noreply.github.com>
2022-10-18 14:06:02 -0700

    Add files via upload

---
+++ MoonKnight/src/Moonknight.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Moonknight {
+	public static void movie() {
+		System.out.print("iron man");
+	}
+	public static void main(String[] args){
+		show();
+		movie();
+	}
+	public static void show() {
+		if(!thisShow.equals("moonknight"));
+		show();
+	}
+}
